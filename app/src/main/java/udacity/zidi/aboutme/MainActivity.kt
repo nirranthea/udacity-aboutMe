43592acc20b11f3eab5cc614cb295d8d00fe9dbc
zidi nirranthea <nirranthea@gmail.com>
2019-07-21 18:35:20 +0700

    use data binding

--- app/src/main/java/udacity/zidi/aboutme/MainActivity.kt
+++ app/src/main/java/udacity/zidi/aboutme/MainActivity.kt
@@ -1,34 +1,37 @@
 package udacity.zidi.aboutme
 
 import android.content.Context
-import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import udacity.zidi.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Agus")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
             addNickName(it)
         }
     }
 
     private fun addNickName(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
-
+        binding.apply {
+//            nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         //Hide the KEYBOARD
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken,0)
---
+++ app/src/main/java/udacity/zidi/aboutme/MyName.kt
@@ -0,0 +1,4 @@
+package udacity.zidi.aboutme
+
+data class MyName (var name: String = "",
+                   var nickname: String = "")
