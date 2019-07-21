2dec8911a900783a317f012a28002a7e89b43ff1
zidi nirranthea <nirranthea@gmail.com>
2019-07-21 17:47:04 +0700

    till done button

--- app/src/main/java/udacity/zidi/aboutme/MainActivity.kt
+++ app/src/main/java/udacity/zidi/aboutme/MainActivity.kt
@@ -1,12 +1,36 @@
 package udacity.zidi.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
+
+        //Hide the KEYBOARD
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
